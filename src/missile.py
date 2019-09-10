5ab7bbdd2b17a6c49789ec6efda822711090ad72
Mikhail Tovarnov <misha198921rip@gmail.com>
2019-09-10 19:29:09 +0300

    test add

---
+++ src/missile.py
@@ -0,0 +1,3 @@
+class Missile(object):
+    def __init__(self):
+        pass
---
+++ tests/test_missile.py
@@ -0,0 +1,16 @@
+import sys
+import pytest
+import os
+import sys
+import numpy as np
+from pytest import approx
+
+wd = os.path.abspath(__file__) # os.path.dirname(os.path.dirname(os.getcwd()))
+wd = os.path.dirname(os.path.dirname(wd))
+sys.path.append(wd+"\\src\\")
+
+from missile import Missile
+
+def test_missile_constructor():
+    m = Missile()
+    assert m is not None
