a9e61a99704728c7214c5c4a4637dc4f746e404f
TovarnovM <misha198921rip@gmail.com>
2021-04-15 00:24:56 +0300

    env done + examples

--- src/cydrone/drone.pyx
+++ src/cydrone/drone.pyx
@@ -1,7 +1,7 @@
 from easyvec.vectors cimport Vec2, real
 import numpy as np
 cimport numpy as np
-from libc.math cimport sin, cos
+from libc.math cimport sin, cos, fmax, pi
 
 
 
@@ -12,25 +12,65 @@ cpdef double get_T_T0(double h_km):
     return (get_rho(h_km) / 1.22544656e+00)**(1/3)
 
 
+cpdef Vec2 get_B(double delta_t, Vec2 A, Vec2 vA):
+    return A.add_vec(vA.mul_num(delta_t / 3))
+
+cpdef Vec2 get_C(double delta_t, Vec2 D, Vec2 vD): 
+    return  D.sub_vec(vD.mul_num(delta_t / 3))
+
+cpdef double get_max_a(double delta_t, Vec2 A, Vec2 B, Vec2 C, Vec2 D):
+    cdef Vec2 a1 = (C.sub_vec(B)).mul_num(6).sub_vec((B.sub_vec(A)).mul_num(6))
+    cdef Vec2 a2 = (D.sub_vec(C)).mul_num(6).sub_vec((C.sub_vec(B)).mul_num(6))
+    return fmax(a1.len(), a2.len())/delta_t/delta_t
+
+cpdef (double, double) get_min_max_v(double delta_t, Vec2 A, Vec2 B, Vec2 C, Vec2 D, int n=42):
+    cdef Vec2 BA = B.sub_vec(A)
+    cdef Vec2 CB = C.sub_vec(B)
+    cdef Vec2 DC = D.sub_vec(C)
+    cdef double min_v = 1e13
+    cdef double max_v = 0
+    cdef int i
+    cdef double dt = 1.0 / (n-1)
+    cdef double t, vel_len
+    for i in range(n):
+        t = i * dt
+        vel_len = (BA.mul_num(3*(1-t)*(1-t)/delta_t).add_vec(CB.mul_num(6*t*(1-t)/delta_t)).add_vec(DC.mul_num(3*t*t/delta_t))).len() 
+        # print(t, vel_len)
+        if vel_len < min_v:
+            min_v = vel_len
+        if vel_len > max_v:
+            max_v = vel_len
+    return min_v, max_v
+
+
 cdef class Drone2d:
-    cdef public double m, J, F_max, L, Cx, t
+    @classmethod
+    def get_DJI(cls):
+        m = 5.0
+        L = 0.5
+        J = m*L**2
+        return cls(m=m, J=J, F_max=42, L=L, Cx=0.2, g=9.81, mu_omega=0.9)
+
+    cdef public double m, J, F_max, L, Cx, t, g, mu_omega
     cdef public double[:] state, tmp1, tmp2, tmp3, tmp4
-    # [0, 1, 2,  3,  4,     5]
-    # [x, y, Vx, Vy, alpha, omega]
+    # [0, 1, 2,  3,  4,     5,     6]
+    # [x, y, Vx, Vy, alpha, omega, t]
     
-    def __init__(self, m, J, F_max, L, Cx):
+    def __init__(self, m, J, F_max, L, Cx, g, mu_omega):
         self.m = m
         self.J = J
         self.F_max = F_max
         self.L = L
         self.Cx = Cx
-        n = 6
+        self.g = g
+        n = 7
         self.state = np.zeros(n)
         self.tmp1 = np.zeros(n)
         self.tmp2 = np.zeros(n)
         self.tmp3 = np.zeros(n)
         self.tmp4 = np.zeros(n)
         self.t = 0
+        self.mu_omega = mu_omega
 
     cpdef void fill_dy(self, double t, double[:] y, double F1, double F2, double[:] dy):
         cdef double rho = get_rho(y[1]/1000)
@@ -54,10 +94,11 @@ cdef class Drone2d:
         dy[0] = y[2]                    # dx = vx
         dy[1] = y[3]                    # dy = vy
         dy[2] = (F_Cx_x + F_x)/self.m        # dvx = ax
-        dy[3] = (F_Cx_y + F_y)/self.m - 9.81 # dvy = ay
+        dy[3] = (F_Cx_y + F_y)/self.m - self.g # dvy = ay
         dy[4] = y[5]                    # dalpha = omega
-        dy[5] = (F2 - F1)*self.L/self.J      # domega
-        # print(f'rho{rho} F_coeff{F_coeff} F1{F1} F2{F2} V{V} F_Cx{F_Cx} F_Cx_x{F_Cx_x} F_Cx_y{F_Cx_y} F_x{F_x} F_y{F_y}')
+        dy[5] = (F2 - F1)*self.L/self.J - dy[5] * self.mu_omega     # domega
+        dy[6] = 1   # dt
+        # print(f'rho={rho} F_coeff={F_coeff} F1={F1} F2={F2} V={V} F_Cx={F_Cx} F_Cx_x={F_Cx_x} F_Cx_y={F_Cx_y} F_x={F_x} F_y={F_y}')
 
     cpdef void step(self, double F1, double F2, double tau=0.1, int n=10):
         """
@@ -66,6 +107,7 @@ cdef class Drone2d:
         """
         cdef double dt = tau / n
         cdef size_t i, j
+        # print(f'before {np.array(self.state)}')
         for i in range(n):
             self.fill_dy(self.t, self.state, F1, F2, self.tmp1) # k1
             for j in range(self.tmp1.shape[0]):
@@ -74,6 +116,10 @@ cdef class Drone2d:
             for j in range(self.state.shape[0]):
                 self.state[j] = self.state[j] + dt * self.tmp2[j]
             self.t += dt
+            self.state[6] = self.t
+            self.flip_alpha() 
+            # print(f'after {np.array(self.state)}')
+
 
     cpdef dict to_dict(self):
         cdef dict res = {
@@ -93,12 +139,85 @@ cdef class Drone2d:
         self.state[3] = state_dict['vel'][1]
         self.state[4] = state_dict['alpha']
         self.state[5] = state_dict['omega']
+        self.state[6] = self.t
+
+    cpdef void flip_alpha(self):
+        if self.state[4] > pi:
+            self.state[4] -= 2*pi
+        elif self.state[4] <= -pi:
+            self.state[4] += 2*pi
+
 
     def to_numpy(self):
         return np.array(self.state)
 
     def from_numpy(self, state):
-        self.state[:] = state[:]
+        cdef size_t i 
+        for i in range(self.state.shape[0]):
+            self.state[i] = state[i]
+        self.t = self.state[6]
+
+    cpdef (double, double, double) get_vmin_vmax_amax(self, double delta_t, Vec2 pos_trg, Vec2 vel_trg, int n_vel_search=25):
+        cdef Vec2 D = pos_trg
+        cdef Vec2 A = Vec2(self.state[0], self.state[1])
+        cdef Vec2 velA = Vec2(self.state[2], self.state[3])
+        cdef Vec2 B = get_B(delta_t, A, velA)
+        cdef Vec2 C = get_C(delta_t, D, vel_trg)
+        cdef double amax = get_max_a(delta_t, A, B, C, D)
+        cdef double vmin, vmax
+        vmin, vmax = get_min_max_v(delta_t, A, B, C, D, n_vel_search)
+        return vmin, vmax, amax
+
+    def get_traject(self, delta_t, pos_trg, vel_trg, n_points=100):
+        ts = np.linspace(0,1, n_points)
+        cdef Vec2 A = Vec2(self.state[0], self.state[1])
+        cdef Vec2 velA = Vec2(self.state[2], self.state[3])
+        cdef Vec2 D = Vec2(pos_trg[0], pos_trg[1])
+        cdef Vec2 B = get_B(delta_t, A, velA)
+        cdef Vec2 C = get_C(delta_t, D, Vec2(vel_trg[0], vel_trg[1]))
+        
+        return np.array([
+            (1-t)**3 * A + 3*t*(1-t)**2 * B + 3*t*t*(1-t)*C + t**3 * D
+            for t in ts
+        ])
+
+    def get_traject_vels(self, delta_t, pos_trg, vel_trg, n_points=100):
+        ts = np.linspace(0,1, n_points)
+        cdef Vec2 A = Vec2(self.state[0], self.state[1])
+        cdef Vec2 velA = Vec2(self.state[2], self.state[3])
+        cdef Vec2 D = Vec2(pos_trg[0], pos_trg[1])
+        cdef Vec2 B = get_B(delta_t, A, velA)
+        cdef Vec2 C = get_C(delta_t, D, Vec2(vel_trg[0], vel_trg[1]))
+        
+        return np.array([
+            (3*(1-t)**2*(B-A) + 6*t*(1-t)*(C-B) + 3*t**2*(D-C))/delta_t
+            for t in ts
+        ])
+
+    @property
+    def vel(self):
+        return Vec2(self.state[2], self.state[3])
+    
+    @property
+    def vel_np(self):
+        return np.array([self.state[2], self.state[3]])
+
+
+    @property
+    def pos(self):
+        return Vec2(self.state[0], self.state[1])
+
+    @property
+    def pos_np(self):
+        return np.array([self.state[0], self.state[1]])
+
+    @property
+    def alpha(self):
+        return self.state[4]
+
+    @property
+    def omega(self):
+        return self.state[5]
 
 
 cpdef real test(int a):
--- src/drone_gym.py
+++ src/drone_gym.py
@@ -1,16 +1,263 @@
 import numpy as np
 from cydrone.drone import Drone2d
-from easyvec import Vec2
+from easyvec import Vec2, Mat2
+from scipy.optimize import minimize
 
-d = Drone2d(m=1, J=2, F_max=3, L=1, Cx=0)
-print(d.to_dict())
+
+
 
 class DroneGym:
-    def __init__(self, drone: Drone2d, pos0: Vec2, pos_trg: Vec2, vel_len_trg: float):
+    @classmethod
+    def make(cls, name):
+        drone = Drone2d.get_DJI()
+        return cls(
+            drone=drone, 
+            pos0=Vec2(-2000, 20), 
+            pos_trg=Vec2(2000,1500), 
+            vel_trg_len=1.0
+        )
+
+    def __init__(self, drone: Drone2d, pos0: Vec2, pos_trg: Vec2, vel_trg_len: float, **kwargs):
         self.time_curr = 0.0
         self.drone = drone
-        self.pos0 = pos0.copy()
+        self.drone.from_dict(dict(
+            t=self.time_curr,
+            pos = pos0,
+            vel=(0,0),
+            alpha=0,
+            omega=0
+        ))
         self.pos_trg = pos_trg.copy()
-        self.vel_len_trg = vel_len_trg
+        self.vel_trg_len = vel_trg_len
+        self.state0 = self.drone.to_numpy()
+
+        self.tau = kwargs.get('tau', 0.25)
+        self.n_4_step = kwargs.get('n_4_step', 10)
+        self.vel_max = kwargs.get('vel_max', 33.0)
+        self.omega_max = kwargs.get('omega_max', 23.0)
+        self.a_trg_max = kwargs.get('a_trg_max', 13.0)
+        self.trg_radius =  kwargs.get('trg_radius', 0.7)
+        self.xy_bounds = kwargs.get('xy_bounds', ((-5000, 5000), (-10, 3000)))
+        self.obs_min = np.array(kwargs.get('obs_max', 
+            (self.xy_bounds[1][0], -np.pi, -np.pi, 0, -np.pi, 0, -self.omega_max) ))
+        self.obs_max = np.array(kwargs.get('obs_max', 
+            (self.xy_bounds[1][1], np.pi, np.pi, self.xy_bounds[0][1] - self.xy_bounds[0][0], np.pi, self.vel_max, self.omega_max) ))
+        self.history = []
+        self.record_history = False
+
+
+    def set_state(self, state):
+        self.drone.from_numpy(state)
+        self.time_curr = self.drone.t
+
+    def reset(self):
+        self.set_state(self.state0)
+        self.history = []
+        return self.get_observ()
+
+    def get_state(self):
+        return self.drone.to_numpy()
+
+    def get_observ(self, state=None, normalize=True):
+        """np.ndarray
+        [0, 1,     2,      3, 4, 5, 6]
+        [H, alpha, beta_v, D, phi, V, omega]
+
+        :param state: state np.ndarray, defaults to None
+        :type state: [type], optional
+        """
+        state0 = None
+        if state:
+            state0 = self.get_state()
+            self.set_state(state)
+        
+        # drone_dict = self.drone.to_dict()
+        H = self.drone.pos.y
+        alpha = self.drone.alpha
+        D_vec = self.pos_trg - self.drone.pos
+        D = D_vec.len()
+        V = self.drone.vel.len()
+        
+        beta_v = Vec2(1,0).angle_to(self.drone.vel.rotate(-alpha))
+        phi =  Vec2(1,0).angle_to(D_vec.rotate(-alpha))
+        omega = self.drone.omega
+
+        if state0:
+            self.set_state(state0)
+        
+        res = np.array([H, alpha, beta_v, D, phi, V, omega])
+        if normalize:
+            res = (res - self.obs_min) / (self.obs_max - self.obs_min)
+        return res
+
+    def get_delta_t_alpha(self):
+        try:
+            def denorm(x, drone, pos_trg, vel_trg_len, a_max):
+                delta_t, alpha_trg = x
+                vA = drone.vel
+                t1 = (pos_trg-drone.pos).len() / (0.5 * (vel_trg_len + vA.len()))
+                delta_t = delta_t * t1
+                return delta_t, alpha_trg
+
+            def minim_foo(x, drone, pos_trg, vel_trg_len, a_max, v_max):
+                delta_t, alpha_trg = denorm(x, drone, pos_trg, vel_trg_len, a_max)
+                if delta_t < 1e-6:
+                    return 1e10
+                vA = drone.vel
+                vel_trg = Vec2(vel_trg_len, 0).rotate(alpha_trg)
+                vmin ,vmax, amax = drone.get_vmin_vmax_amax(delta_t, pos_trg, vel_trg)
+                shtraf = 0
+                vminAD = min(vA.len(), vel_trg_len) * 0.5
+                vmaxAD = max(vA.len(), vel_trg_len, v_max) 
+                if vmin < vminAD:
+                    shtraf += 10
+                if vmax > vmaxAD:
+                    shtraf += 10
+                if amax > a_max:
+                    shtraf += 30
+                return delta_t * (shtraf + 1) + shtraf
+            
+            res = minimize(
+                minim_foo, 
+                (1,0), 
+                args=(self.drone, self.pos_trg, self.vel_trg_len, self.a_trg_max, self.vel_max),
+                method='Nelder-Mead', 
+                options={
+                    'maxfev': 100,
+                    'initial_simplex': np.array([[1,0], [2.5, -1], [0.5, 1]])
+                    }
+                )
+            delta_t, alpha = denorm(res.x, self.drone, self.pos_trg, self.vel_trg_len, self.a_trg_max)
+            return delta_t, alpha
+        except Exception as e:
+            print(f'При подсчете get_delta_t была ошибка {e}')
+            return 999, 0
+
+
+    def is_done(self):
+        drone_pos = self.drone.pos
+        if (drone_pos - self.pos_trg).len() < self.trg_radius:
+            if self.drone.vel.len() <= self.vel_trg_len:
+                return True, 100, {'result': 'success'}
+        if drone_pos.y < self.xy_bounds[1][0] or drone_pos.y > self.xy_bounds[1][1]:
+            return True, -100, {'result': 'out of Y bounds'}
+        if drone_pos.x < self.xy_bounds[0][0] or drone_pos.x > self.xy_bounds[0][1]:
+            return True, -100, {'result': 'out of X bounds'}
+        if abs(self.drone.omega) > self.omega_max:
+            return True, -100, {'result': f'omega too mutch {self.drone.omega}'}
+        return False, 0, {}
+
+
+    def step(self, actions):
+        """observation_, reward, done, info
+
+        :param action: [description]
+        :type action: [type]
+        """
+        delta_t0, alpha = self.get_delta_t_alpha()
+        F1 = actions[0]
+        F2 = actions[1]
+        F1 = 1 if F1 > 1 else 0 if F1 < 0 else F1
+        F2 = 1 if F2 > 1 else 0 if F2 < 0 else F2
+        self.drone.step(F1, F2, self.tau, self.n_4_step)
+        self.time_curr = self.drone.t
+        delta_t1, alpha = self.get_delta_t_alpha()
+
+        reward = delta_t0 - delta_t1
+        observation_ = self.get_observ()
+        done, add_reward, info = self.is_done()
+        reward += add_reward
+        if self.record_history:
+            self.history.append({
+                'state': self.get_state(),
+                'actions': np.array(actions),
+                'delta_t1': delta_t1,
+                'alpha': alpha,
+                'observation_': observation_,
+                'reward': reward,
+                'done': done,
+                'info': info
+            })
+
+        return observation_, reward, done, info
+
+    def plot(self, ax, state=None, drone_mashtb=None, vec_mashtb=None,
+            actions=None, ideal_traj=False, **kwargs):
+        state0 = None
+        if state:
+            state0 = self.get_state()
+            self.set_state(state)
+
+        if drone_mashtb:
+            L = self.drone.L * drone_mashtb
+            points = np.array([
+                (-0.3, 0), (-0.3, -0.1), (-1.7, -0.1), (-1.7, 0.1), (-0.3, 0.1), (0,0),
+                (0.3, 0.1),(1.7, 0.1), (1.7, -0.1), (0.3, -0.1), (0.3, 0)
+            ]) * L
+            pos = self.drone.pos_np
+
+            M_rot = np.asarray(Mat2.from_angle(self.drone.alpha).as_np())
+            points =  points @ M_rot+ pos
+
+            drone_kw = {'lw': 1, 'color': 'darkblue'}
+            drone_kw = dict(drone_kw, **kwargs.get('drone_kw', {}))
+            ax.plot(points[:,0], points[:,1], **drone_kw)
+
+            if actions is not None:
+                F1 = np.array([0, L * actions[0]])
+                F1 =  F1 @ M_rot
+                F01 =  np.array([-L, 0]) @M_rot + pos
+                actions_kw = {'width': 0.05 * L, 'color': 'red'}
+                actions_kw = dict(actions_kw, **kwargs.get('actions_kw', {}))
+                ax.arrow(F01[0], F01[1], F1[0], F1[1], **actions_kw)
+
+                F2 = np.array([0, L * actions[1]])
+                F2 =  F2 @ M_rot
+                F02 = np.array([L, 0]) @ M_rot + pos
+                ax.arrow(F02[0], F02[1], F2[0], F2[1], **actions_kw)     
+
+        if vec_mashtb:
+            vel = self.drone.vel * vec_mashtb
+            pos = self.drone.pos
+
+            vel_kw = {'width': 0.05 * vel.len(), 'color': 'green'}
+            vel_kw = dict(vel_kw, **kwargs.get('vel_kw', {}))
+            ax.arrow(pos.x, pos.y, vel.x, vel.y, **vel_kw)
+        
+        if ideal_traj:
+            delta_t, alpha = self.get_delta_t_alpha()
+            vel_trg = Vec2(self.vel_trg_len, 0).rotate(alpha)
+            traject = self.drone.get_traject(delta_t, self.pos_trg, vel_trg)
+
+            ideal_traj_kw = {'ls': ':', 'color': 'green'}
+            ideal_traj_kw = dict(ideal_traj_kw, **kwargs.get('ideal_traj_kw', {}))
+            ax.plot(traject[:,0], traject[:,1], **ideal_traj_kw)
+
+        if state0:
+            self.set_state(state0)
+        
+        
+if __name__ == '__main__':
+    gym = DroneGym.make('ha')
+    a = gym.reset()
+    a1 = gym.step([1,1])
+    a2 = gym.step([1,0.5])
+    a3 = gym.step([1,0.5])
+    for i in range(100):
+        a4 = gym.step([1,1])
+
+ 
+
+        
+        
+
+
+        
+
+
+    
+
+    
+
 
     
